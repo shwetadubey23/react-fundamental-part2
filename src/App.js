280ac78a3b1b3b363a07faef87a908fcfc699e0e
shwetadubey23 <shwetadubey0246@gmail.com>
2024-05-26 16:38:10 +0530

    submit password

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import './App.css';
 
 function App() {
   const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
   function handleUserName(e) {
     setUserName(e.target.value)
   }
+  function handelPassword(e) {
+    setPassword(e.target.value)
+  }
+
   function handelformSubmit(e) {
-   e.preventDefault()
-console.log('submit');
+    e.preventDefault()
+    console.log('submit');
   }
   return (
     <div className="App">
       <form onSubmit={handelformSubmit}>
-        <label>Name:</label>
-        <input type='text' value={userName} onChange={handleUserName} />
+        <div>
+          <label>User Name:</label>
+          <input type='text' value={userName} onChange={handleUserName} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type='password' value={password} onChange={handelPassword} />
+        </div>
         <button type='submit' >Submit</button>
       </form>
     </div>
