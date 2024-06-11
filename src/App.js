3503c723e36e7c46577f12b87f73bc713575bffa
shwetadubey23 <shwetadubey0246@gmail.com>
2024-06-11 18:12:12 +0530

    reduce extra state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 // import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
--- src/component/SubmitForm.js
+++ src/component/SubmitForm.js
@@ -1,55 +1,41 @@
 import React, { useState } from 'react'
-import '../App.css'
+
 export default function SubmitForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        password: '',
-        gender: ''
-    })
+    const [firstName, setFirstName] = useState('')
+const [lastName, setLastName] = useState('')
+const fullName = firstName + " " + lastName
+// const handelFirstName = (e) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // setFormData({
-        //     name: '',
-        //     email: '',
-        //     phone: '',
-        //     password: '',
-        //     gender: ''
-        // })
-        console.log('formData', formData);
-    }
+// }
+  return (
+    <form>
+<h1>Form</h1>
+<div>
+    <lable htmlFor='firstName'>First Name</lable>
+    <input type='text' id='firstName' name='first' placeholder='First Name' value= {firstName}
+    onChange={(e) => setFirstName(e.target.value) }
+    />
+</div>
 
-    const handleFormData = (e) => {
-        setFormData(prevState => {
-            return { ...prevState, [e.target.id]: e.target.value }
-        })
-    }
-    return (
-        <div>
-            <form className='form' onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input type='text' id='name' placeholder='Name' value={formData.name} onChange={handleFormData} /> <br />
-                <label>Email:</label>
-                <input type='email' id='email' placeholder='Email' value={formData.email} onChange={handleFormData} /> <br />
-                <label>Phone:</label>
-                <input type='text' id='phone' placeholder='Phone' value={formData.phone} onChange={handleFormData} /> <br />
-                <label>Password:</label>
-                <input type='password' id='password' placeholder='Password' value={formData.password} onChange={handleFormData} /> <br />
-                <label htmlFor='male'>Male</label>
-                <input type='radio' name='gender' id='male'
-                    value={formData.gender}
-                    checked={formData.gender === 'male'}
-                    onChange={handleFormData}
-                />
-                <label htmlFor='female'>Female</label>
-                <input type='radio' name='gender' id='female'
-                    value={formData.gender}
-                    checked={formData.gender === 'female'}
-                    onChange={handleFormData} />
-            </form>
-            <button>Submit</button>
-        </div>
-    )
+<div>
+    <lable htmlFor='lastName'>Last Name</lable>
+    <input type='text' id='lastName' name='last' placeholder='Last Name' value={lastName}
+    onChange={(e) => setLastName(e.target.value)}
+    />
+
+</div>
+<br/>
+
+<p>
+    First Name: {firstName}
+</p>
+<p>
+    Last Name: {lastName}
+</p>
+<p>
+    Full Name: {fullName}
+</p>
+    </form>
+    
+  )
 }
