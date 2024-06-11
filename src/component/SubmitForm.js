b48fd38cbecb8c2d4ede4ac44a86dc38b93c0925
shwetadubey23 <shwetadubey0246@gmail.com>
2024-06-11 17:09:24 +0530

    submit-form

--- src/component/SubmitForm.js
+++ src/component/SubmitForm.js
@@ -9,38 +9,45 @@ export default function SubmitForm() {
         gender: ''
     })
 
-    const handelSubmit = (e) => {
-        e.preventDefalut()
-        setFormData('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // setFormData({
+        //     name: '',
+        //     email: '',
+        //     phone: '',
+        //     password: '',
+        //     gender: ''
+        // })
+        console.log('formData', formData);
     }
 
-    const handelFormData = (e) => {
+    const handleFormData = (e) => {
         setFormData(prevState => {
             return { ...prevState, [e.target.id]: e.target.value }
         })
     }
     return (
         <div>
-            <form className='form' onSubmit={handelSubmit}>
+            <form className='form' onSubmit={handleSubmit}>
                 <label>Name:</label>
-                <input type='text' id='name' placeholder='Name' value={formData.name} onChange={handelFormData} /> <br />
+                <input type='text' id='name' placeholder='Name' value={formData.name} onChange={handleFormData} /> <br />
                 <label>Email:</label>
-                <input type='email' id='email' placeholder='Email' value={formData.email} onChange={handelFormData} /> <br />
+                <input type='email' id='email' placeholder='Email' value={formData.email} onChange={handleFormData} /> <br />
                 <label>Phone:</label>
-                <input type='text' id='phone' placeholder='Phone' value={formData.phone} onChange={handelFormData} /> <br />
+                <input type='text' id='phone' placeholder='Phone' value={formData.phone} onChange={handleFormData} /> <br />
                 <label>Password:</label>
-                <input type='passwrd' id='password' placeholder='Password' value={formData.password} onChange={handelFormData} /> <br />
-                <label>Mail</label>
-                <input type='radio' name='gender' id='mail' 
-                value={formData.gender} 
-                checked={formData.gender === 'mail'}
-                onChange={handelFormData}
-                 />
-                <label>Femail</label>
-                <input type='radio' name='gender' id='femail'
-                 value={formData.gender} 
-                 checked = {formData.gender === 'femail'}
-                onChange={handelFormData} />
+                <input type='password' id='password' placeholder='Password' value={formData.password} onChange={handleFormData} /> <br />
+                <label htmlFor='male'>Male</label>
+                <input type='radio' name='gender' id='male'
+                    value={formData.gender}
+                    checked={formData.gender === 'male'}
+                    onChange={handleFormData}
+                />
+                <label htmlFor='female'>Female</label>
+                <input type='radio' name='gender' id='female'
+                    value={formData.gender}
+                    checked={formData.gender === 'female'}
+                    onChange={handleFormData} />
             </form>
             <button>Submit</button>
         </div>
