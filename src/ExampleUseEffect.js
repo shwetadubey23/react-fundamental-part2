ac0ab13f5703de47c4eabae8a223acb1af02d918
shwetadubey23 <shwetadubey0246@gmail.com>
2024-06-15 14:01:13 +0530

    useEffect-hook fr sideEffect

--- src/ExampleUseEffect.js
+++ src/ExampleUseEffect.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 export default function ExampleUseEffect () {
+    console.log('Component Render');
     useEffect(() => {
         console.log('side Effect');
-    }, [])
+    }, []) // when we pass [] empty array (called dependency array) useEffect call only once on component render 
 const [num, setNum] = useState(0)
 const increasValue = () => {
     setNum(num + 1)
