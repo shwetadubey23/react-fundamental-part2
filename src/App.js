6b3d9d9809f6c29d863d7169ed543dba52a09e97
shwetadubey23 <shwetadubey0246@gmail.com>
2024-06-22 17:14:18 +0530

    useReducer for complex state instid of useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
+
 function App() {
+  function reducer(state, action) {
+    if (action.type === "INCRMENT") {
+      return { ...state, count: state.count + 1 }
+    }
+
+    if (action.type === "Decrement") {
+      return { ...state, count: state.count - 1 }
+    }
+
+    if (action.type === "RESET") {
+      return { ...state, count: 0 }
+    }
+
+    return state
+  }
+
+  const initialState = { count: 0 }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleIncrease = () => {
+    dispatch({
+      type: "INCRMENT"
+    })
+  }
+
+  const handleDecrease = () => {
+    dispatch({
+      type: "Decrement"
+    })
+  }
+
+  const handleReset = () => {
+    dispatch({
+      type: "RESET"
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UseReducer</h1>
+      <hr />
+      <h2>Counter Application</h2>
+      <h2>{state.count}</h2>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 }
