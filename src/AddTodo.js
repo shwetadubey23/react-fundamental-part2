d4dfd982bb6f7b61b92faa1dfbea4ec357dde7d4
shwetadubey23 <shwetadubey0246@gmail.com>
2024-06-24 18:27:46 +0530

    add todo with useReducer

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,6 +1,6 @@
 import React, { useState }  from 'react'
 import './App.css'
-export default function AddTodo({todoState, dispatch}) { 
+export default function AddTodo({ dispatch}) { 
   
 const [addTodo, setAddTodo] = useState('')
 
